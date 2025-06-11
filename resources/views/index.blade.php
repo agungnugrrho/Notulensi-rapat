11fa8f9dd94355c4c005bf55d1acd2825d5c10c2
Agungrh <ngrh15.an@gmail.com>
2025-06-11 09:54:01 +0700

    update tabel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Data Notulensi Rapat</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="icon1.css">
 
     {{-- css icon info --}}
     <style>
         .custom-swal-icon {
-          width: 50px !important;  /* Lebar ikon lebih kecil */
-          height: 50px !important; /* Tinggi ikon lebih kecil */
+            width: 50px !important;
+            /* Lebar ikon lebih kecil */
+            height: 50px !important;
+            /* Tinggi ikon lebih kecil */
         }
 
         .custom-swal-popup {
-          padding: 10px !important; /* Kurangi padding agar tampilan lebih compact */
+            padding: 10px !important;
+            /* Kurangi padding agar tampilan lebih compact */
         }
     </style>
     {{-- batas css --}}
 
 </head>
+
 <body style="font-family: Rockwell, Georgia, Serif;">
 
     {{-- form index --}}
@@ -47,6 +53,7 @@
                             <th>Tempat</th>
                             <th>Pimpinan</th>
                             <th>Agenda</th>
+                            <th>coba</th>
                             <th>Aksi</th>
                         </tr>
                     </thead>
@@ -63,13 +70,17 @@
                                     <td class="text-center align-middle">{{ $notulen->chairman }}</td>
                                     <td class="text-center align-middle">{{ $notulen->agenda }}</td>
                                     <td>
-                                        <a href="{{ route('notulen.edit', $notulen->id) }}" class="btn btn-sm btn-primary" title="Edit">
+                                        <a href="{{ route('notulen.edit', $notulen->id) }}"
+                                            class="btn btn-sm btn-primary" title="Edit">
                                             <i class="bi bi-pencil-square fs-5"></i>
                                         </a>
-                                        <a href="#" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="loadNotulen({{ $notulen->id }})" title="Detail">
+                                        <a href="#" class="btn btn-sm btn-primary" data-bs-toggle="modal"
+                                            data-bs-target="#staticBackdrop" onclick="loadNotulen({{ $notulen->id }})"
+                                            title="Detail">
                                             <i class="bi bi-info-circle-fill fs-5"></i>
                                         </a>
-                                        <a href="{{ route('absen.index', $notulen->id) }}" class="btn btn-sm btn-primary" title="Absen">
+                                        <a href="{{ route('absen.index', $notulen->id) }}"
+                                            class="btn btn-sm btn-primary" title="Absen">
                                             <i class="bi bi-card-checklist fs-5"></i>
                                         </a>
                                     </td>
@@ -83,34 +94,36 @@
     </div>
     {{-- batas form index --}}
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     {{-- deskripsi --}}
     <script>
-function loadNotulen(id) {
-    fetch('/notulen/' + id)
-    .then(response => response.json())
-    .then(data => {
-        let dateObj = new Date(data.dateInput);
-        let timeObj = new Date("1970-01-01T" + data.timeInput);
+        function loadNotulen(id) {
+            fetch('/notulen/' + id)
+                .then(response => response.json())
+                .then(data => {
+                    let dateObj = new Date(data.dateInput);
+                    let timeObj = new Date("1970-01-01T" + data.timeInput);
 
-        let formattedDate = dateObj.toLocaleDateString('id-ID', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric'
-        });
+                    let formattedDate = dateObj.toLocaleDateString('id-ID', {
+                        day: '2-digit',
+                        month: 'long',
+                        year: 'numeric'
+                    });
 
-        let formattedTime = timeObj.toLocaleTimeString('id-ID', {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false
-        });
+                    let formattedTime = timeObj.toLocaleTimeString('id-ID', {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false
+                    });
 
-        // Menampilkan modal SweetAlert2 dengan data rapat
-        Swal.fire({
-        title: "<strong>Detail Rapat</strong>",
-        html: `
+                    // Menampilkan modal SweetAlert2 dengan data rapat
+                    Swal.fire({
+                        title: "<strong>Detail Rapat</strong>",
+                        html: `
             <table class="table table-borderless text-start">
                 <tr><td><b>Waktu</b></td>
                 <td>:</td>
@@ -137,57 +150,58 @@ function loadNotulen(id) {
             </tbody>
             </table>
         `,
-        icon: "info",
-        width: '1000px',
-        height: '1200px',
-        showCloseButton: true,
-        focusConfirm: false,
-        showConfirmButton: false,
-        allowOutsideClick: false, // Klik di luar tidak akan menutup modal
-        allowEscapeKey: false,
-        customClass: {
-    popup: 'custom-swal-popup',
-    icon: 'custom-swal-icon' // Tambahkan kelas khusus ke ikon
-}
+                        icon: "info",
+                        width: '1000px',
+                        height: '1200px',
+                        showCloseButton: true,
+                        focusConfirm: false,
+                        showConfirmButton: false,
+                        allowOutsideClick: false, // Klik di luar tidak akan menutup modal
+                        allowEscapeKey: false,
+                        customClass: {
+                            popup: 'custom-swal-popup',
+                            icon: 'custom-swal-icon' // Tambahkan kelas khusus ke ikon
+                        }
 
-        });
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        Swal.fire({
-        title: "Terjadi Kesalahan",
-        text: "Gagal mengambil data notulen.",
-        icon: "error",
-        confirmButtonText: "OK"
-        });
-    });
-}
+                    });
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    Swal.fire({
+                        title: "Terjadi Kesalahan",
+                        text: "Gagal mengambil data notulen.",
+                        icon: "error",
+                        confirmButtonText: "OK"
+                    });
+                });
+        }
     </script>
     {{-- batas deskripsi --}}
 
     {{-- notifikasi sukses --}}
     <script>
         // Tampilkan notifikasi sukses pada halaman index setelah berhasil simpan
-        @if(session('message'))
-    Swal.fire({
-        title: "Sukses!",
-        text: "{{ session('message') }}",
-        icon: "success",
-        timer: 2000,
-        showConfirmButton: false
-    });
-    @endif
-</script>
-{{-- batas notifikasi --}}
+        @if (session('message'))
+            Swal.fire({
+                title: "Sukses!",
+                text: "{{ session('message') }}",
+                icon: "success",
+                timer: 2000,
+                showConfirmButton: false
+            });
+        @endif
+    </script>
+    {{-- batas notifikasi --}}
 
 </body>
+
 </html>
 
 
 
 
-    <!-- Modal -->
-    {{-- <div class="modal fade modal-lg" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<!-- Modal -->
+{{-- <div class="modal fade modal-lg" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -232,7 +246,7 @@ function loadNotulen(id) {
         </div>
     </div> --}}
 
-    {{-- <script>
+{{-- <script>
         function loadNotulen(id) {
         fetch('/notulen/' + id)
         .then(response => response.json())
@@ -262,8 +276,8 @@ function loadNotulen(id) {
     }
         </script> --}}
 
-        {{-- button icon --}}
-            {{-- <td>
+{{-- button icon --}}
+{{-- <td>
             a href="{{ route('notulen.edit', $notulen->id) }}">
             <i class="bi bi-pencil-square fs-3" style="margin: 0.5rem; color: #0d6efd"></i>
             </a>
@@ -274,4 +288,4 @@ function loadNotulen(id) {
             <i class="bi bi-card-checklist fs-3" style="margin: 0.5rem; color: #0d6efd"></i>
             </a>
             </td> --}}
-        {{-- batas button icon --}}
+{{-- batas button icon --}}
